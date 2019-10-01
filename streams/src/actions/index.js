4d2e9d1bad42f939948e58755a3d115042c10e54
erikmjohnson <erikarisma98@gmail.com>
2019-09-30 18:20:39 -0700

    Section 20 Completed

--- streams/src/actions/index.js
+++ streams/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -27,6 +28,7 @@ export const createStreams = formValues => async (dispatch, getState) => {
   const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
   //programmatic navigation - going to another page when an event happens
 };
 
@@ -37,15 +39,16 @@ export const fetchStreams = () => async  dispatch => {
 };
 
 export const fetchStream = id => async dispatch => {
-  const response = await streams.get(`/streams/:${id}`);
+  const response = await streams.get(`/streams/${id}`);
 
   dispatch({ type: FETCH_STREAM, payload: response.data })
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/:${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const deleteStream = id => async dispatch => {
